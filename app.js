4aae26af8bfabe00af9a188afa0fcd91f4609b04
sebas099 <sebastian.falc@gmail.com>
2025-02-05 17:41:55 -0300

    Agregada función para insertar nombres en el arreglo

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let nombreAgregado = document.getElementById("amigo").value;
+
+    if (!validarEntrada(nombreAgregado, "nombre")) {
+        return;
+    }
+    
+    amigos.push(nombreAgregado);
+    document.getElementById("amigo").value = "";
+}
+
+function validarEntrada(dato, tipoDato) {
+    if (dato.trim() === '') {
+        alert(`Por favor, inserte un ${tipoDato}.`);
+        return false;
+    }
+    return true;
+}
+//TODO Functions:
+// Add names: Type a friend's name in the text field and click "Add" to add it to the visible list and validate the input;
+//If the text field is empty, an alert will appear asking for a valid name. The entered names will appear in a list below 
+//the input field.
+
+// Random draw: Click the "Draw a Friend" button to randomly select a name from the list and display it on the page. 
