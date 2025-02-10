9e437e29664bafb03b5a8b9164e3b75b7737ae4a
sebas099 <sebastian.falc@gmail.com>
2025-02-10 19:17:58 -0300

    Código refactorizado.

--- app.js
+++ app.js
@@ -2,47 +2,28 @@ let amigos = [];
 limpiarCampo("amigo");
 
 function agregarAmigo() {
-    
     let nombreAgregado = document.getElementById("amigo").value;
 
-    if (!validarEntrada(nombreAgregado, "nombre")) {
+    if (nombreAgregado.trim() === '') {
+        alert("Por favor, inserte un nombre.");
         return;
     }
 
     amigos.push(nombreAgregado);
     limpiarCampo("amigo");
-    agregarEnLista(amigos[amigos.length - 1], "listaAmigos");
-}
-
-function agregarEnLista(elementoEnArray, lista) {
-    let elemento = document.createElement("li");
-    
-    elemento.textContent = elementoEnArray;
-    document.getElementById(lista).appendChild(elemento);
+    document.getElementById("listaAmigos").innerHTML += `<li>${amigos[amigos.length - 1]}</li>`;
 }
 
 function sortearAmigo() {
-    tamañoArray = amigos.length;
+    let tamañoArray = amigos.length;
     
     if(tamañoArray == 0) {
         alert("La lista de amigos  está vacía.")
         return;
     }
-    limpiarCampo(listaGanadores);
-    let indiceAleatorio = Math.floor(Math.random() * tamañoArray);
-    let listaGanadores = document.getElementById("resultado");
-    let ganador = document.createElement("li");
-    
-    ganador.textContent = amigos[indiceAleatorio];
-    listaGanadores.appendChild(ganador);
-}
 
-function validarEntrada(dato, tipoDato) {
-    if (dato.trim() === '') {
-        alert(`Por favor, inserte un ${tipoDato}.`);
-        return false;
-    }
-    return true;
+    let indiceAleatorio = Math.floor(Math.random() * tamañoArray);
+    document.getElementById("resultado").innerHTML = `<li>${amigos[indiceAleatorio]}</li>`; 
 }
 
 function limpiarCampo(IdCampo){
